eeca4c23c13680e9652cce68c1dea1deb2bc1986
tonya-newer <tonya@newerconsulting.com>
2025-09-27 15:18:11 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 // src/main.tsx
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./routes";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
